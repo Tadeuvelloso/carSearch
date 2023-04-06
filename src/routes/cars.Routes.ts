26af013744d79c1b51711815e96cd729069b718b
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-04-06 15:48:09 -0300

    feat: cars routes

---
+++ src/routes/cars.Routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import carsControllers from "../controllers/carsControllers.js";
+import { validationSchema } from "../middlewares/carMiddleware.js";
+import { carSchema } from "../schemas/carSchema.js";
+
+const carsRoutes = Router();
+
+carsRoutes.post("/cars",validationSchema(carSchema), carsControllers.createCar);
+carsRoutes.get("/cars", carsControllers.getAllCars);
+carsRoutes.get("/cars/:id", carsControllers.getCar);
+carsRoutes.put("/cars/:id", carsControllers.updateCar);
+carsRoutes.delete("/cars/:id", carsControllers.deleteCar);
+
+export default carsRoutes;
