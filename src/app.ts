7f3e92c97dfe5b4f2e39a5167375151914ac4345
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-04-07 18:13:05 -0300

    feat: import to errormiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { json, Response, Request } from "express";
 import carsRoutes from "./routes/cars.Routes.js";
+import cors from "cors";
+import { handleApplicationErrors } from "./middlewares/errorMiddleware.js";
 
 const app = express();
-app.use(json());
-app.use(carsRoutes)
-
-app.get("/health", (req: Request, res: Response) => {
-    res.send("Funcionado!");
-}); 
+app
+  .use(cors())
+  .use(json())
+  .get("/health", (req: Request, res: Response) => {res.send("Funcionado!")})
+  .use(carsRoutes)
+  .use(handleApplicationErrors)
  
-app.listen(5000, () => {
-    console.log("Server is up and running on port 5000");
+const port = 5000
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
 })
