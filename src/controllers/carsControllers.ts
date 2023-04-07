aac86bbd35367a099b813017a1348ffd81dd5113
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-04-07 18:13:54 -0300

    refac: cacth error handling

--- src/controllers/carsControllers.ts
+++ src/controllers/carsControllers.ts
@@ -1,56 +1,56 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Car } from "../protocols/Car.js";
 import carService from "../services/carsService.js";
 
 
-async function createCar(req: Request, res: Response) {
+async function createCar(req: Request, res: Response, next: NextFunction) {
     const car = req.body as Car;
     try{
         await carService.createCar(car);
         res.sendStatus(201);
     } catch(err) {
-        return res.status(500).send(err.message);
+        next(err);
     }
 }
 
-async function getAllCars(req: Request, res: Response) {
+async function getAllCars(req: Request, res: Response, next: NextFunction) {
     try{
         const allCars = await carService.findAllCars();
         res.send(allCars)
     } catch(err) {
-        return res.status(500).send(err.message);
+        next(err);
     }
 }
 
-async function getCar(req: Request, res: Response) {
+async function getCar(req: Request, res: Response, next: NextFunction) {
     const {id} = req.params;
     try{
         const car = await carService.findOneCar(Number(id));
         res.send(car);
     } catch(err) {
-        return res.status(500).send(err.message);
+        next(err);
     }
 }
 
 
-async function updateCar(req: Request, res: Response) {
+async function updateCar(req: Request, res: Response, next: NextFunction) {
     const car = req.body as Car;
     const { id } = req.params;
     try{
         await carService.updateOneCar(car, Number(id));
         res.sendStatus(200)
     } catch(err) {
-        return res.status(500).send(err.message);
+        next(err);
     }
 }
 
-async function deleteCar(req: Request, res: Response) {
+async function deleteCar(req: Request, res: Response, next: NextFunction) {
     const { id } = req.params;
     try{
         await carService.deleteOneCar(Number(id));
         res.sendStatus(200);
     } catch(err) {
-        return res.status(500).send(err.message);
+        next(err);
     }
 }
 
